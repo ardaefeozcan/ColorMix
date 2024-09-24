16d5d8b1939f80cc30ee454eaa4d881174f711ff
ardaefeozcan <ardaefe.ozcan@hisarschool.k12.tr>
2024-09-24 23:41:10 +0300

    Initial Commit

---
+++ ColorMixTests/ColorMixTests.swift
@@ -0,0 +1,17 @@
+//
+//  ColorMixTests.swift
+//  ColorMixTests
+//
+//  Created by Arda Efe Özcan on 24.09.2024.
+//
+
+import Testing
+@testable import ColorMix
+
+struct ColorMixTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ColorMixUITests/ColorMixUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ColorMixUITestsLaunchTests.swift
+//  ColorMixUITests
+//
+//  Created by Arda Efe Özcan on 24.09.2024.
+//
+
+import XCTest
+
+final class ColorMixUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
